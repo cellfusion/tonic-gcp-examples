4da42b60329d00bb40f65453212f1d2ac1548abd
Mk-10:cellfusion <makoto@cellfusion.jp>
2020-06-13 10:17:06 +0900

    add sample tts

--- examples/text_to_speech.rs
+++ examples/text_to_speech.rs
@@ -66,7 +66,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }))
         .await?;
 
-    println!("RESPONSE = {:?}", response);
+    println!("Response:{:?}", response);
 
     Ok(())
 }
